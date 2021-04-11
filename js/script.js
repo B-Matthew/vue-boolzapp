51610a947a5d3f2e7b5d0ecc8cd00b5ebacc1e2f
Matthew <matsgeco92@hotmail.it>
2021-04-11 21:46:53 +0200

    Mileston 1 2 3 finite

--- js/script.js
+++ js/script.js
@@ -6,45 +6,139 @@ function initVue() {
     data: {
       contacts: [
         {
-          name: 'Matteo',
+          name: 'Matteo S.',
           avatar: 'img/Salvini.jpg',
           visible: true,
           messages: [
             {
-              date: '10/01/2020 15:30:55',
-              text: 'Hai portato a spasso il cane?',
+              date: '10/01/2020',
+              hours: '15:30',
+              text: 'Hai preso la mascherina?',
               status: 'sent'
             },
             {
-              date: '10/01/2020 15:50:00',
-              text: 'Ricordati di dargli da mangiare',
+              date: '10/01/2020',
+              hours: '15:50',
+              text: 'No, non mi serve!',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020',
+              hours: ' 16:15',
+              text: 'Guarda che non puoi andare in giro senza!! :D',
               status: 'sent'
             },
             {
-              date: '10/01/2020 16:15:22',
-              text: 'Tutto fatto!',
+              date: '10/01/2020',
+              hours: '15:50',
+              text: 'Ah no?? Non posso?',
+              status: 'received'
+            },
+          ],
+        },
+        {
+          name: 'Gandalf',
+          avatar: 'img/Gandalf.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020',
+              hours: '15:30',
+              text: 'Dove sei??',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020',
+              hours: '15:50',
+              text: 'Dovevamo trovarci qui alle 9.00 in punto',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020',
+              hours: ' 16:15',
+              text: 'Arrivo arrivo!',
+              status: 'received'
+            },
+            {
+            date: '10/01/2020',
+            hours: ' 16:15',
+            text: 'Ho fatto pausa per comprare della roba per dopo! :)',
+            status: 'received'
+          }
+          ],
+        },
+        {
+          name: 'Pippo',
+          avatar: 'img/Pippo.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020',
+              hours: '15:30',
+              text: 'Aigloah!',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020',
+              hours: '15:50',
+              text: '??? Tutto bene?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020',
+              hours: ' 16:15',
+              text: 'Scusa, ho sempre questo singhiozzo che mi parte a caso... ',
               status: 'received'
             }
           ],
         },
+        {
+          name: 'Maestro Sgarbi',
+          avatar: 'img/Sgarbi.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020',
+              hours: '15:30',
+              text: 'Salve Maestro, o letto il suo libro!',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020',
+              hours: '15:50',
+              text: 'O LETTO??? CAPRA,CAPRA,CAPRA,CAPRA,CAPRA,CAPRA,CAPRA!',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020',
+              hours: ' 16:15',
+              text: 'ops',
+              status: 'sent'
+            }
+          ],
+        },
+
         {
           name: 'Harry',
           avatar: 'img/harry.jpg',
           visible: true,
           messages: [
             {
-              date: '20/03/2020 16:30:00',
-              text: 'Ciao come stai?',
+              date: '20/03/2020',
+              hours: ' 16:30',
+              text: 'Hai preso tutto per stasera? Dai che dobbiamo preparare',
               status: 'sent'
             },
             {
-              date: '20/03/2020 16:30:55',
-              text: 'Bene grazie! Stasera ci vediamo?',
+              date: '20/03/2020',
+              hours: ' 16:30',
+              text: "Sisi ho trovato le gelatineeee.. C'è cioccolato e menta piperita. E c'è anche spinaci, fegato e trippa. George giura che ne ha trovata una al gusto di caccole!",
               status: 'received'
             },
             {
-              date: '20/03/2020 16:35:00',
-              text: 'Mi piacerebbe ma devo andare a fare laspesa.',
+              date: '20/03/2020',
+              hours: ' 16:35',
+              text: 'Bleah, quella roba piace solo a te',
               status: 'sent'
             }
           ],
@@ -55,26 +149,72 @@ function initVue() {
           visible: true,
           messages: [
             {
-              date: '28/03/2020 10:10:40',
-              text: 'La Marianna va in campagna',
+              date: '28/03/2020',
+              hours: ' 10:10',
+              text: 'MONELLA!',
               status: 'received'
             },
             {
-              date: '28/03/2020 10:20:10',
-              text: 'Sicuro di non aver sbagliato chat?',
+              date: '28/03/2020',
+              hours: '10:20',
+              text: 'Hai sbagliato chat ancora......',
               status: 'sent'
             },
             {
-              date: '28/03/2020 16:15:22',
+              date: '28/03/2020',
+              hours: ' 16:18',
               text: 'Ah scusa!',
+              status: 'received'
+            },
+            {
+              date: '28/03/2020',
+              hours: ' 16:18',
+              text: 'MONELLO!',
               status: 'received'
             }
           ],
         },
-      ]
+      ],
+      indice: 0,
+      val: "",
+    },
+
+    methods: {
+      indexContact: function(index) {
+        this.indice = index;
+      },
+
+      addMsg: function() {
+        const msgSent = {
+          date: '28/03/2020',
+          hours: ' 16:18',
+          text: this.val,
+          status: 'sent'
+        }
+        this.val="";
+        this.contacts[this.indice].messages.push(msgSent);
+        setTimeout(this.answerInt,1000);
+      },
+
+      answerInt: function () {
+         const answer = {
+           date: '28/03/2020',
+           hours: ' 16:18',
+           text: "Ok",
+           status: 'received'
+         }
+         this.contacts[this.indice].messages.push(answer);
+
+      }
     }
   });
 }
+
+
+
+
+
+
 
 function init() {
 
